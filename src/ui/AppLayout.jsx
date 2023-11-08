3b6abaa8ac1b9521718e95cf29ef62ba74a0e469
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-08 11:05:14 -0700

    implement a Loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,27 @@
 import Footer from './Footer';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
-import { Outlet } from 'react-router-dom';
+import Loader from './Loader';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isLoading = navigation.state === 'loading';
+  // {state: 'idle', location: undefined, …}
+  // {state: 'loading', location: {…},…}
+
   return (
-    <>
+    <div className='layout'>
+      {isLoading && <Loader />}
       <Header />
+      {/* <main>{!isLoading ? <Outlet /> : <Loader />}</main> */}
       <main>
-        <Outlet />
+        <Outlet />{' '}
       </main>
       <CartOverview />
       <Footer />
-    </>
+    </div>
   );
 }
 
