8f7bb2cf19465a9d5afe9f83351c63c9007c5382
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-07 22:46:40 -0700

    Create routes with createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import CreateUser from './features/user/CreateUser';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
+import OrderItem from './features/order/OrderItem';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/user',
+    element: <CreateUser />,
+    // loader: teamLoader,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/order',
+    element: <Order />,
+    children: [
+      {
+        path: 'new',
+        element: <CreateOrder />,
+      },
+      {
+        path: ':id',
+        element: <OrderItem />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hi</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
@@ -9,3 +52,5 @@ export default App;
 // npm run dev
 
 // npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
+
+// npm i react-router-dom@6
