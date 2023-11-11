e9fba4cb59a07d8967f527227dc116d08470944b
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-10 22:30:39 -0700

    Create LinkButton component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,14 +1,19 @@
-function Button({ children, disabled }) {
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to }) {
+  const classes =
+    'tracking-wide rounded-full bg-stone-400 uppercase font-semibold inline-block px-4 py-3 hover:bg-stone-300 border border-gray-300 hover:border-stone-400 transition-colors duration-300 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4';
+
+  if (to) {
+    return (
+      <Link to={to} className={classes}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      disabled={disabled}
-      className='tracking-wide rounded-full
-      bg-stone-400 uppercase font-semibold inline-block px-4 
-      py-3 hover:bg-stone-300 
-      border border-gray-300 hover:border-stone-400 transition-colors duration-300
-      focus:outline-none disabled:cursor-not-allowed
-      sm:px-6 sm:py-4'
-    >
+    <button disabled={disabled} className={classes}>
       {children}
     </button>
   );
