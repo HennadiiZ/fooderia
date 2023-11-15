1b536abf03e018620fb64d390c9bb29c80e6db1a
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-11-15 11:47:34 -0700

    Prevent UserName component from unnes unnecessary rendering

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -3,6 +3,10 @@ import { useSelector } from 'react-redux';
 function UserName() {
   const username = useSelector((state) => state.user.username);
 
+  if (!username) {
+    return null;
+  }
+
   return (
     <div className='md:inline-block hidden text-sm font-semibold'>
       {username}
